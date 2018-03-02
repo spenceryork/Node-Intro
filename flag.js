88e6d150475242868a025589fb1fe14ac2cab533
Spencer York <spenceryork@Bernies-MacBook-Pro.local>
2018-03-02 08:54:48 -0600

    flag exercise complete

---
+++ flag.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+const chalk = require('chalk');
+
+let flagBar = "                                                              ";
+let stars
+
+console.log(chalk.bgBlue(chalk.white(" ", "*", " ", "*", " ", "*", " ", "*", " ", "*", " ", "*", " ")) + chalk.bgRed("                                     "));
+console.log(chalk.bgBlue(chalk.white(" ", " ", "*", " ", "*", " ", " ", " ", "*", " ", "*", " ", " ")) + chalk.bgWhite("                                     "));
+console.log(chalk.bgBlue(chalk.white(" ", "*", " ", "*", " ", "*", " ", "*", " ", "*", " ", "*", " ")) + chalk.bgRed("                                     "));
+console.log(chalk.bgBlue(chalk.white(" ", " ", "*", " ", "*", " ", " ", " ", "*", " ", "*", " ", " ")) + chalk.bgWhite("                                     "));
+console.log(chalk.bgBlue(chalk.white(" ", "*", " ", "*", " ", "*", " ", "*", " ", "*", " ", "*", " ")) + chalk.bgRed("                                     "));
+console.log(chalk.bgBlue(chalk.white(" ", " ", "*", " ", "*", " ", " ", " ", "*", " ", "*", " ", " ")) + chalk.bgWhite("                                     "));
+console.log(chalk.bgBlue(chalk.white(" ", "*", " ", "*", " ", "*", " ", "*", " ", "*", " ", "*", " ")) + chalk.bgRed("                                     "));
+console.log(chalk.bgWhite(flagBar));
+console.log(chalk.bgRed(flagBar));
+console.log(chalk.bgWhite(flagBar));
+console.log(chalk.bgRed(flagBar));
+console.log(chalk.bgWhite(flagBar));
+console.log(chalk.bgRed(flagBar));
+
+
+// console.log(chalk.bgWhite);
+// console.log(chalk.bgRed);
+// console.log(chalk.bgWhite);
+// console.log(chalk.bgRed);
+// console.log(chalk.bgWhite);
+// console.log(chalk.bgRed);
+// console.log(chalk.bgWhite);
+// console.log(chalk.bgRed);
+// console.log(chalk.bgWhite);
+// console.log(chalk.bgRed);
+// console.log(chalk.bgWhite);
+// console.log(chalk.bgRed);
