93a801a2461a5630b1a12fa59603330c62d735d7
Spencer York <spenceryork@Bernies-MacBook-Pro.local>
2018-03-02 15:24:02 -0600

    Synchronous File IO exercise complete

---
+++ file-io.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+const { readFileSync } = require("fs");
+
+const file = process.argv[2];
+
+if(file) {
+    try {
+        const data = readFileSync(file);
+        process.stdout.write(data);
+    } catch (err) {
+        console.log("Didn't work", error);
+    }
+}
