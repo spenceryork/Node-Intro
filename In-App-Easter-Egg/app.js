4cf55cab23df96dbedd92db17ec6a5a93a6a02f1
Spencer York <spencerwyork@gmail.com>
2018-03-15 11:37:43 -0500

    routes and easter egg working

---
+++ In-App-Easter-Egg/app.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const appRoutes = require('./routes/appRoutes');
+const app = express();
+
+const currentTime = ( req, res, next ) => {
+    let date = new Date();
+    req.currentTime = date.toISOString();
+    next();
+}
+
+const setEgg = ( req, res, next ) => {
+    process.stdout.write(`
+    You found the Easter Egg at ${req.currentTime}
+    ",ggadddd8888888bbbbaaa,_
+    ,ad888,      Y88,      Y888baa,
+  ,dP"  "Y8b,      "Y8b,      "Y8888ba,
+ ,88      "Y88b,      "Y8ba,       "Y88Ya,
+,P88b,      "Y88b,       "Y8ba,_       ""8a,
+,P'"Y88b,        "Y88b,        "Y8ba,_      Ya,
+8'    "Y88b,        ""Y8ba,         ""Y8ba,_   8,
+8b       "Y88b,         ""Y8ba,_         ""Y88baaY
+88b,        "Y88ba,         ""Y88ba,_         ""P
+8Y88ba,        ""Y88ba,_         ""Y88ba,,_    ,P'
+b,"Y88ba,         ""Y88baa,_         """Y888bd"
+b, "Y88ba,_         ""Y888baa,_         ,8"
+ 8,   ""Y88ba,_         """Y8888baaaaaP"
+  Ya,     ""Y888ba,_           "d88P"
+    "Yb,,_     ""Y888baa,__,,adP""'
+        """YYYY8888888PPPP"""'"
+    `)
+    next();
+}
+
+// middlewares
+
+app.use(currentTime);
+app.use(setEgg);
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/see-chickens', (req, res) => {
+    res.sendFile(__dirname + '/public/see-chickens.html');
+});
+
+app.get('/see-eggs', (req, res) => {
+    res.sendFile(__dirname + '/public/see-eggs.html');
+    req.setEgg;
+});
+
+
+
+app.listen(8080, () => {
+    console.log('listening on port 8080');
+});
---
+++ In-App-Easter-Egg/routes/appRoutes.js
@@ -0,0 +1,20 @@
+const { Router } = require('express');
+const router = Router();
+const path = require('path');
+
+router.get('./home', (req, res) => {
+    console.log("Going Home");
+    res.sendFile(path.join(__dirname, '../public', "index"));
+});
+
+router.get('./see-chickens', (req, res) => {
+    console.log("Going to see some chickens");
+    res.sendFile(path.join(__dirname, '../public', "see-chickens.html"));
+});
+
+router.get('./see-eggs', (req, res) => {
+    console.log("Going to see come eggs");
+    res.sendFile(path.join(__dirname, '../public', "see-eggs.html"));
+});
+
+module.exports = router;
